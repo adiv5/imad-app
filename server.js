c1fc228bd75ba58da1ce00fd1878b4f8d82db946
Aditya Vartak <aadivartak@gmail.com>
2017-10-14 22:07:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('/pg2', function (req, res) {
+  res.send("Hello i have formed new URL");
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
